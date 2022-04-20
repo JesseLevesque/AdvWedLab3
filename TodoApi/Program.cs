f3301568c8ab0f94159ba14440e77ef1dd81e496
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2022-04-20 13:18:04 -0400

    todoitems api all but patch

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,8 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Controllers;
+using TodoApi.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+var dbmsVersion = new MariaDbServerVersion(builder.Configuration.GetValue<string>("DBMSVersion"));
+var connString = builder.Configuration.GetConnectionString("TodoDbContext");
+
+
+builder.Services.AddDbContext<TodoDbContext>(opt => opt.UseMySql(connString, dbmsVersion));
+
 builder.Services.AddControllers();
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,8 +26,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -21,5 +35,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
 
 app.Run();
