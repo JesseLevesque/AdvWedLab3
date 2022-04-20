6d86103a3d5455427a4bb64687cd25eb0d9e1db0
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2022-04-20 12:19:20 -0400

    init todoapi

---
+++ TodoApi/Model/TodoItem.cs
@@ -0,0 +1,13 @@
+namespace TodoApi.Model;
+
+
+public class TodoItem
+{
+
+	public uint TodoItemId { get; set; }
+	public string? Task { get; set; }
+	public string? Instructions { get; set; }
+	public bool IsComplete { get; set; } = false;
+
+
+}
